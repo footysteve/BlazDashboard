2a007a0eb8275b7918f14bf24245527739e983ed
footysteve <41590886+footysteve@users.noreply.github.com>
2018-09-04 20:40:40 +0100

    Add files via upload

---
+++ src/BlazorDash.Shared/CompetitionBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDash.Shared
+{
+    public class CompetitionBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Abbreviation { get; set; }
+        public long UtcStartDate { get; set; }
+        public long UtcEndDate { get; set; }
+        public long LocalStartDate { get; set; }
+        public long LocalEndDate { get; set; }
+        
+    }
+}
---
+++ src/BlazorDash.Shared/FootballLeagueCompetition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorDash.Shared
+{
+    public partial class FootballLeagueCompetition : CompetitionBase
+    {
+        public int TeamCount { get; set; }
+        public int PlayerCount { get; set; }
+        public int MatchCount { get; set; }
+        public int Weeks { get; set; }
+        public int CurrentWeek { get; set; }
+        public string GoverningBody { get; set; }
+        public string Country { get; set; }
+        
+    }
+}
